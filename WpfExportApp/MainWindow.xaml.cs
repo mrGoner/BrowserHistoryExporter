4209c551f7b7a5f06a753885e613cde845ea2afe
Roman <genom90@gmail.com>
2018-04-25 02:46:53 +0300

    Начал работу над визуальным стилем приложения

--- WpfExportApp/MainWindow.xaml.cs
+++ WpfExportApp/MainWindow.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System.Windows;
+using Microsoft.Win32;
+using WpfExportApp.ViewModels;
 
 namespace WpfExportApp
 {
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
